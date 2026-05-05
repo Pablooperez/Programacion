b78e952b4cf56896018cba14244a6682ecc5ef6c
pablo pérez <pablo.perez.martinez99@gmail.com>
2026-05-05 09:32:38 +0200

    Practica Sublime Formularios

---
+++ PRO01-EJERCICIOS/Pruebas/pruebasMain.java
@@ -0,0 +1,9 @@
+class pruebasMain {
+	public static void main(String[] args) {
+		pruebas prueba1 = new pruebas();
+
+		prueba1.setBounds(100,100,500,500);
+		prueba1.setVisible(true);
+		prueba1.setLocationRelativeTo(null);
+	}
+}
